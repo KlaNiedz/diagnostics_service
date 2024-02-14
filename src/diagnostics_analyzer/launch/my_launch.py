b25b539657d299b99f0afe1cb0303744a8d0cb13
klaudia <klaudianiedzi@gmail.com>
2024-02-14 20:08:53 +0100

    another changes with params

--- src/diagnostics_analyzer/launch/my_launch.py
+++ src/diagnostics_analyzer/launch/my_launch.py
@@ -1,15 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="diagnostics_analyzer",
-            executable="my_publisher",
-            name="my_publisher",
-            output='screen',
-            emulate_tty=True,
-            parameters=[os.path.join(os.getcwd(), 'my_publisher_params.yaml')]
+    ld = LaunchDescription()
+
+    config = os.path.join(
+        get_package_share_directory('diagnostics_analyzer'),
+        'config',
+        'my_publisher_params.yaml'
         )
-    ])
+        
+    node=Node(
+        package = 'diagnostics_analyzer',
+        name = 'my_publisher',
+        executable = 'my_publisher',
+        parameters = [config]
+    )
+
+    ld.add_action(node)
+    return ld
+
